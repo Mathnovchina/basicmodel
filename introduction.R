59334fd1f3777334949c1c7edefd89034eb374eb
Mathnovchina <matthieu.bordenave@etu.utc.fr>
2023-09-18 17:16:52 +0200

    Init

---
+++ introduction.R
@@ -0,0 +1,20 @@
+##intermediate variables 
+
+
+##time derivatives
+
+
+
+##initial values
+
+
+
+##parameters
+
+
+
+#_______________________________________________________________________________________________________________________________#
+##time
+begin = 0                      # Note that time can be used as a variable "t"
+end = 84
+by = 0.05
